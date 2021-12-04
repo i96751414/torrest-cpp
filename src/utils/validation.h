46f98838b7ad30f42b1ce3a943e1a5102aa3fb29
i96751414 <i96751414@gmail.com>
2021-12-04 23:21:43 +0000

    Update validation to always create messages with actual values

--- src/utils/validation.h
+++ src/utils/validation.h
@@ -1,6 +1,8 @@
 #ifndef TORREST_VALIDATION_H
 #define TORREST_VALIDATION_H
 
+#include <string>
+
 #define _VALIDATE1(var, v1) if (_GET_OPERATOR v1 (var _GET_VALIDATOR v1)) throw torrest::utils::ValidationException("'" #var "' " _GET_MESSAGE v1);
 #define _VALIDATE2(var, v1, ...) _VALIDATE1(var, v1) _VALIDATE1(var, __VA_ARGS__)
 #define _VALIDATE3(var, v1, ...) _VALIDATE1(var, v1) _VALIDATE2(var, __VA_ARGS__)
@@ -10,10 +12,10 @@
 #define _GET_VALIDATOR(a, b, c) b
 #define _GET_MESSAGE(a, b, c) c
 
-#define LT(l) (!, < l, "must be less than " # l)
-#define GT(l) (!, > l, "must be greater than " # l)
-#define LTE(l) (!, <= l, "must be less than or equal to " # l)
-#define GTE(l) (!, >= l, "must be greater than or equal to " # l)
+#define LT(l) (!, < l, "must be less than " + std::to_string(l))
+#define GT(l) (!, > l, "must be greater than " + std::to_string(l))
+#define LTE(l) (!, <= l, "must be less than or equal to " + std::to_string(l))
+#define GTE(l) (!, >= l, "must be greater than or equal to " + std::to_string(l))
 #define NOT_EMPTY() (, .empty(), "cannot be empty")
 #define VALIDATE(var, ...) _GET_MACRO(__VA_ARGS__, _VALIDATE3, _VALIDATE2, _VALIDATE1)(var, __VA_ARGS__)
 
@@ -22,6 +24,8 @@ namespace torrest { namespace utils {
     class ValidationException : public std::runtime_error {
     public:
         explicit ValidationException(const char *pMessage) : std::runtime_error(pMessage) {}
+
+        explicit ValidationException(const std::string &pMessage) : std::runtime_error(pMessage) {}
     };
 
 }}
